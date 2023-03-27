0433af3d7011f3b73f742721712620a77b7eb11f
Adyna Lima <adynalsl@gmail.com>
2023-03-27 17:27:16 -0300

    feat: construindo o js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-document.querySelector('#principa1').addEventListener('click')
+const enviar = document.querySelector('.enviar')
+const cartao1 = document.querySelector('.principal')
+const cartao2 = document.querySelector('.secundaria')
+const nota = document.querySelector('.numeros')
+const avaliar = document.querySelector('#avaliacao')
+
+enviar.addEventListener ("click", () => {
+    cartao2.classList.remove("hidden")
+    cartao1.style.display = 'none'
+})
