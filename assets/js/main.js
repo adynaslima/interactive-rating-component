3df7d942c10dc1159efe630457bfb5188447d6ba
Adyna Lima <adynalsl@gmail.com>
2023-03-28 09:36:13 -0300

    feat: javascript teste

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,16 @@
-const enviar = document.querySelector('.enviar')
-const cartao1 = document.querySelector('.principal')
-const cartao2 = document.querySelector('.secundaria')
-const nota = document.querySelector('.numeros')
-const avaliar = document.querySelector('#avaliacao')
+const enviar = document.querySelector(".enviar")
+const cartao1 = document.querySelector(".principal")
+const cartao2 = document.querySelector(".secundaria")
+const nota = document.querySelector(".numeros")
+const avaliacao = document.querySelectorAll("#avaliacao")
 
 enviar.addEventListener ("click", () => {
     cartao2.classList.remove("hidden")
-    cartao1.style.display = 'none'
+    cartao1.style.display = "none"
+})
+
+nota.forEach((numero) => { 
+    numero.addEventListener("click", () => {
+        avaliacao.innerHTML = numero.innerHTML
+    })
 })
